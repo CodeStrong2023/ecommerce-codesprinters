2a3d246e49c5f2e352a9258a25c91732651bec90
Juanbarrera12 <juanbarrerase@gmail.com>
2024-11-06 13:09:51 -0300

    Subo la parte del carrito

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Login from "./pages/Login/Login";
+import Cart from "./pages/Cart/Cart";
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
       <Footer />
     </>
